d5e82ca0c7e7566ef843a3dd5de69e89ba03f53f
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-26 16:21:54 +0300

    Updates

---
+++ precache-manifest.b02183362d704e4aad9152fa79ce719a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb34bd1bb641701a0c3f76852d9b91b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a7099240aa65f1593fb3",
+    "url": "./static/css/main.5d59f344.chunk.css"
+  },
+  {
+    "revision": "c86e17cb030a39369fd1",
+    "url": "./static/js/2.f6671328.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f6671328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7099240aa65f1593fb3",
+    "url": "./static/js/main.dbb0b58c.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
